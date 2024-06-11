e46c62b797bae00781c58786dfde0bc1bd1182da
fatmab0 <fatmabasar2000@gmail.com>
2024-06-11 22:54:45 +0300

    Add project files.

---
+++ SuperMarket.Web/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SuperMarket.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "Hello world";
+
+        }
+    }
+}
